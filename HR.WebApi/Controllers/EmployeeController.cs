ab1793153c1c2fe00daf205215d4732b1946d351
sumit <31644611+serenesumit@users.noreply.github.com>
2018-11-13 14:19:27 +0530

    update url

--- HR.WebApi/Controllers/EmployeeController.cs
+++ HR.WebApi/Controllers/EmployeeController.cs
@@ -17,7 +17,7 @@ using System.Web.Http.Cors;
 namespace HR.WebApi.Controllers
 {
     [EnableCors(origins: "*", headers: "*", methods: "*")]
-    [RoutePrefix("api/Employee")]
+    [RoutePrefix("apiemployee")]
     public class EmployeeController : ApiController
     {
 
